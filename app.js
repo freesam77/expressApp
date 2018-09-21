0c6adf197390ef16644ab4ccbd34c0c4c7345fd9
Samuel Razali <samuelrazali@gmail.com>
2018-09-21 10:16:29 +1000

    add page to display search and result from OMDB movie database.

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 let express = require("express");
 let app = express();
+let bodyParser = require("body-parser");
+var request = require('request');
 
+// Global Variables
 
-// respond with "hello world" when a GET request is made to the homepage
+let data = [
+    {magic: "Fire Element", desc: "Fire, Fira, Firaga"},
+    {magic: "Blizzard Element", desc: "Blizzard, Blizzara, Blizzaga"},
+    {magic: "Thunder Element", desc: "Thunder, Thundara, Thundaga"},
+    {magic: "Cure Element", desc: "Cure, Cura, Curaga"},
+    {magic: "Gravity Element", desc: "Gravity, Gravira, Graviga"},
+    {magic: "Time Element", desc: "Stop, Stopra, Stopga"},
+    {magic: "Wind Element", desc: "Aero, Aerora, Aeroga"}
+];
+
+// ==> The extended option allows to choose between parsing the URL-encoded data with the querystring library (when false) or the qs library (when true). 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(express.static("public"))
 app.set("view engine","ejs");
+
+// Home page
 app.get('/', function (req, res) {
-    res.send('Omae wa mou...shindeiru!!!!')
+    res.send('Welcome to the Home Page!')
   })
 
 app.get('/bye', function (req, res) {
@@ -14,20 +31,21 @@ app.get('/bye', function (req, res) {
     res.send('Bye bye!!')
 })
 
+// GET and POST data library
 app.get('/kh', function (req, res) {
-    let data = [
-        {magic: "Fire Element", desc: "Fire, Fira, Firaga"},
-        {magic: "Blizzard Element", desc: "Blizzard, Blizzara, Blizzaga"},
-        {magic: "Thunder Element", desc: "Thunder, Thundara, Thundaga"},
-        {magic: "Cure Element", desc: "Cure, Cura, Curaga"},
-        {magic: "Gravity Element", desc: "Gravity, Gravira, Graviga"},
-        {magic: "Time Element", desc: "Stop, Stopra, Stopga"},
-        {magic: "Wind Element", desc: "Aero, Aerora, Aeroga"}
-    ];
     res.render("kh",{data: data});
 })
 
-// speak according
+app.post('/newpost', function (req, res){
+    let newPost = req.body;
+    data.push({
+        magic: newPost.magic + " Element",
+        desc: newPost.desc
+    });
+    res.redirect("/kh");
+})
+
+// Display matching string according to set of data
 app.get('/speak/:animal', function (req, res) {
     let animal = req.params.animal;
     let speakLibrary = {
@@ -42,7 +60,7 @@ app.get('/speak/:animal', function (req, res) {
     res.send("<h2>The " +  animal + " says</h2><h1>" + speaking + "</h1>");
 })
 
-// repeat as many times as "num" entered.
+// Repeat as many times as "num" entered.
 app.get('/repeat/:subsection/:number', function (req, res) {
     let subname = req.params.subsection;
     let num = parseInt(req.params.number);
@@ -53,8 +71,26 @@ app.get('/repeat/:subsection/:number', function (req, res) {
     res.send("You have entered the page of <strong>" + subname + "</strong> " + num + " times.</br>" + finalword);
 })
 
+// =====
+// Request to OMDB using the API key provided by Colt Steele in Fullstack Web Dev tut @ udemy...
+// =====
+app.get('/search',function(req,res){
+    res.render("search")
+})
 
-// for any other routes
+// Display result of the search page
+app.get('/result',function(req,res){
+    let search = req.query.search;
+    let url = 'http://www.omdbapi.com/?s=ironman&apikey=thewdb';
+    let url2 = 'http://www.omdbapi.com/?s=' + search + '&apikey=thewdb';
+    request(url2, function (error, response, body) {
+        let parsedData = JSON.parse(body);
+        let data = parsedData.Search;
+        res.render("result",{data: data})
+    })
+})
+
+// Page not found for the rest of the route
 app.get('*', function (req, res) {
     res.send('The Page you requested doesn\'t exist! Get a LIFE!')
 })
